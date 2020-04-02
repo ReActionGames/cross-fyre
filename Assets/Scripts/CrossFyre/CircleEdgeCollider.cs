a9a8fe72a0678257a123a022f732e97121e330b9
Josh Hendershot <hendershotjoshr+git@gmail.com>
2020-04-02 11:20:48 -0400

    Added circle arena sprite and collider

---
+++ Assets/Scripts/CrossFyre/CircleEdgeCollider.cs
@@ -0,0 +1,38 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace CrossFyre
+{
+    [RequireComponent(typeof(EdgeCollider2D))]
+    public class CircleEdgeCollider : MonoBehaviour
+    {
+        [SerializeField] private int numEdges = 16;
+        [SerializeField] private float radius = 5f;
+
+        [Button]
+        private void UpdateCollider()
+        {
+            var edgeCollider = GetComponent<EdgeCollider2D>();
+            if (edgeCollider == null)
+            {
+                Debug.LogError("Cannot update collider. " + gameObject.name + " does not have an edge collider attached!");
+                return;
+            }
+            
+            var points = new Vector2[numEdges + 1];
+
+            for (var i = 0; i < numEdges; i++)
+            {
+                var angle = 2 * Mathf.PI * i / numEdges;
+                var x = radius * Mathf.Cos(angle);
+                var y = radius * Mathf.Sin(angle);
+
+                points[i] = new Vector2(x, y);
+            }
+
+            points[numEdges] = points[0];
+
+            edgeCollider.points = points;
+        }
+    }
+}
