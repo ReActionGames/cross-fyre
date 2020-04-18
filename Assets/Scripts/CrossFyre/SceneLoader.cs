1ec23e5426ca2f22027328513689e782b33a46d2
Josh Hendershot <hendershotjoshr+git@gmail.com>
2020-04-18 18:03:29 -0400

    scene loader works in editor mode

--- Assets/Scripts/CrossFyre/SceneLoader.cs
+++ Assets/Scripts/CrossFyre/SceneLoader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using CrossFyre.GameSettings;
 using Sirenix.OdinInspector;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -33,13 +34,46 @@ namespace CrossFyre
             metaLevel = "MetaLevel";
 
         [SerializeField] private Dictionary<Arena, string> arenaSceneNames;
+        [SerializeField] private string sceneFolderPath = "Assets/Scenes";
 
 
         private IEnumerator Start()
         {
+            yield return LoadSelectedArena(GameSettingsManager.Settings.debugMode);
+        }
+
+#if UNITY_EDITOR
+        [DisableInPlayMode]
+        [Button(ButtonSizes.Large, Name = "Load Selected Arena")]
+        private void LoadSelectedArenaEditor()
+        {
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName(persistent));
+            while (SceneManager.sceneCount > 1)
+            {
+                var index = 0;
+                var scene = SceneManager.GetSceneAt(index);
+                while (scene.name.Equals(persistent))
+                {
+                    index++;
+                    scene = SceneManager.GetSceneAt(index);
+                }
+
+                EditorSceneManager.CloseScene(scene, true);
+            }
+            
+            EditorSceneManager.OpenScene(sceneFolderPath + "/" + metaLevel + ".unity", OpenSceneMode.Additive); 
+
+            EditorSceneManager.OpenScene(sceneFolderPath + "/" + ArenaName + ".unity", OpenSceneMode.Additive);
+
+            SceneManager.SetActiveScene(SceneManager.GetSceneByPath(sceneFolderPath + "/" + ArenaName + ".unity"));
+        }
+#endif
+
+        private IEnumerator LoadSelectedArena(bool debugMode)
+        {
             SceneManager.SetActiveScene(SceneManager.GetSceneByName(persistent));
             yield return StartCoroutine(UnloadAllScenesExceptPersistent());
-            if (GameSettingsManager.Settings.debugMode)
+            if (debugMode)
             {
                 yield return SceneManager.LoadSceneAsync(debug, LoadSceneMode.Additive);
             }
