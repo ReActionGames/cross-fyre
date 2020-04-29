3c974c89a5017fba8f43e82f8429512d4af25349
Josh Hendershot <hendershotjoshr+git@gmail.com>
2020-04-29 17:47:10 -0400

    Fixed restart bug

--- Assets/Scripts/CrossFyre/UI/UiControlPanel.cs
+++ Assets/Scripts/CrossFyre/UI/UiControlPanel.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using CrossFyre.Player;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace CrossFyre.UI
 {
     public class UiControlPanel : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
     {
         public event Action<PointerEventData> BeginDrag, Drag, EndDrag;
+
+        private void OnEnable()
+        {
+            FindObjectOfType<PlayerController>().Health.OnDeath.AddListener(Deactivate);
+        }
+
+        private void OnDisable()
+        {
+            FindObjectOfType<PlayerController>().Health.OnDeath.RemoveListener(Deactivate);
+        }
+
+        private void Deactivate()
+        {
+            GetComponent<Image>().raycastTarget = false;
+        }
 
         public void OnBeginDrag(PointerEventData eventData)
         {
