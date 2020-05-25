56ba291d2ec7be0467f1df1871439f4b5f351307
Josh Hendershot <hendershotjoshr+git@gmail.com>
2020-05-25 13:46:37 -0400

    Created Level Manager script

---
+++ Assets/Scripts/CrossFyre/Level/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using CrossFyre.Gun;
+using UnityEngine;
+
+namespace CrossFyre.Level
+{
+    [Serializable]
+    public class Node
+    {
+        [SerializeField] private GunController gun;
+        [SerializeField] private Vector3 spawnPoint;
+    }
+
+    [Serializable]
+    public class Wave
+    {
+        [SerializeField] private Node[] nodes;
+    }
+
+    public class LevelManager : MonoBehaviour
+    {
+        [SerializeField] private Wave[] waves;
+    }
+}
