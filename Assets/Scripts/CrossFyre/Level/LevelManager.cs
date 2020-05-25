a58c4d9a59af4c09e8fd5d8da74308052714fdc2
Josh Hendershot <hendershotjoshr+git@gmail.com>
2020-05-25 14:34:12 -0400

    Created waves and nodes

--- Assets/Scripts/CrossFyre/Level/LevelManager.cs
+++ Assets/Scripts/CrossFyre/Level/LevelManager.cs
@@ -1,25 +1,65 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using CrossFyre.Gun;
+using Lean.Pool;
+using UnityEditor;
 using UnityEngine;
 
 namespace CrossFyre.Level
 {
     [Serializable]
-    public class Node
+    public struct Node
     {
-        [SerializeField] private GunController gun;
-        [SerializeField] private Vector3 spawnPoint;
+        public float delay;
+        public GunController gun;
+        public Vector3 spawnPoint;
     }
 
     [Serializable]
     public class Wave
     {
+        public Node[] Nodes => nodes;
+        public float Delay => delay;
+
+        [SerializeField] private float delay = 1f;
         [SerializeField] private Node[] nodes;
     }
 
     public class LevelManager : MonoBehaviour
     {
         [SerializeField] private Wave[] waves;
+
+        private void Start()
+        {
+            if (waves.Length <= 0) return;
+            
+            StartCoroutine(SpawnWaveAsync(waves[0]));
+        }
+
+        private IEnumerator SpawnWaveAsync(Wave wave)
+        {
+            yield return new WaitForSeconds(wave.Delay);
+
+            foreach (var node in wave.Nodes)
+            {
+                yield return new WaitForSeconds(node.delay);
+                LeanPool.Spawn(node.gun, node.spawnPoint, Quaternion.identity);
+            }
+        }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            foreach (var wave in waves)
+            {
+                foreach (var node in wave.Nodes)
+                {
+                    Gizmos.DrawWireSphere(node.spawnPoint, 0.25f);
+                }
+            }
+        }
+#endif
     }
 }
