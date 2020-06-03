a1a23dbed56d58635e2d3d12cdcde8597fdc90ac
Josh Hendershot <hendershotjoshr+git@gmail.com>
2020-06-03 11:55:47 -0400

    Added script and adjusted assembly defs
    Added DoozyUI scripts to the assembly definition ecosystem

---
+++ Assets/Scripts/CrossFyre/UI/UiLevelMessage.cs
@@ -0,0 +1,22 @@
+﻿using DoozyUI;
+using UnityEngine;
+
+namespace CrossFyre.UI
+{
+    public class UiLevelMessage : MonoBehaviour
+    {
+        [SerializeField] private UIElement messageElement; 
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
